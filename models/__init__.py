7e7f5c2f38c45d29f5f8369de082d7ef72335c53
arjunbhagoji <arjunbhagoji@gmail.com>
2021-06-30 14:43:45 -0500

    name_change

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
 from .cnn import cnnSmallBN, cnnLargeBN, cnnLarge
-from .resnet import resnet18, resnet50
+from .resnet_pyt import resnet18, resnet50
 from .ResNet import ResNet18, ResNet50
 from .MobileNet import MobileNet
