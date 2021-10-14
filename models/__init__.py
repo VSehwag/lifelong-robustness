c771163f3b8fa52e4e2e078f9adb9442f28dbe98
Vikash Sehwag <38088124+VSehwag@users.noreply.github.com>
2021-10-14 14:51:05 -0400

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from .cnn import cnnSmallBN, cnnLargeBN, cnnLarge
 from .resnet_pyt import resnet18, resnet50
 from .ResNet import ResNet18, ResNet50
 from .MobileNet import MobileNet
+from .wide_resnet import wrn_28_4, wrn_28_10, wrn_34_10, wrn_34_20, wrn_70_16
